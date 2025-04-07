9d87570a46000d1858f6e07161b8ede6177580c1
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-07 11:30:49 -0500

    codigo acabado

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,40 @@ int main(void) {
     DDRD |= 0xF0;            // PD4–PD7 como salida (decenas)
     DDRB |= 0x0F;            // PB0–PB3 como salida (unidades)
     
-    DDRD &= ~(0x04);     //  como entrada (botón)
+    DDRD &= ~(0x04);     //  boton para subir
     PORTD |= (0x04);     // Pull-up en PD3 (botón a GND)
     
-    DDRD &= ~(0x08);     // como entrada (botón)
-    PORTD |= (0x08);     // Pull-up en PD3 (botón a GND)
+    DDRD &= ~(0x08);     // boton para bajar
+    PORTD |= (0x08);     // Pull-up en PD4 (botón a GND)
 
+    uint8_t unidades = 0; //declaramos la variable entero sin signo para poder contar en unidades
+    uint8_t decenas = 0; //declaramos las variables para descenas inia en 0 el valor
 
-    uint8_t unidades = 0;
-    uint8_t decenas = 0;
+    while (1) { //base programa
 
-    while (1) {
+        
+        //para el boton que sube numero de 1 en 1
+
         if (!(PIND&0X04)) {     // Si el botón está presionado
             _delay_ms(50);              // Anti-rebote
 
             // Esperar a que se suelte el botón
             while (!(PIND & 0x04)); //mientras el boton este precionado el numero cambiara
+                _delay_ms(50);              // Anti-rebote
 
                 if (unidades >= 9) {
-                    _delay_ms(50);
                     unidades = 0;
-                    decenas++;
+                    decenas++; //sube en una unidad
                 }
                     else {
-                    _delay_ms(50);
-                    unidades++;
+                    unidades++; //sube en una unidad
 
                     }
                     if (decenas > 9) {
-                        _delay_ms(50);
-                        decenas = 0;
+                        decenas = 0; // Asegurarse que las decenas no superen 9
                     }
                 }
+
             //para que retroceda pd3
 
             if (!(PIND & 0x08)) {     // Si el botón en PD3 está presionado
@@ -47,7 +49,7 @@ int main(void) {
                 while (!(PIND & 0x08)); // Esperar a que se suelte el botón
                 _delay_ms(50);
 
-                if (!(unidades == 0 && decenas == 0)) { //
+                if (!(unidades == 0 && decenas == 0)) { // esto es para que se quede firme en 00 y no de vuelta el de unidad
 
                     if (unidades == 0) {
                         unidades = 9;    // Si las unidades están en 0, reiniciar a 9
@@ -56,7 +58,7 @@ int main(void) {
                             decenas--;   // Decrementa las decenas si son mayores que 0
                         }
                     } else {
-                        unidades--;     // Si no, solo decrementa las unidades
+                        unidades--;     // disminuye la unidad
                     }
         
                     if (decenas > 9) {
@@ -65,12 +67,9 @@ int main(void) {
 
                 }
             }
-
-
-    
-            // Mostrar en PD4–PD7 (decenas)
-            PORTD = (PORTD & 0x0F) | (decenas << 4);
-            // Mostrar en PB0–PB3 (unidades)
-            PORTB = (PORTB & 0xF0) | (unidades & 0x0F);
+            // Mostrar en PD4–PD7 (decenas) y en PB0–PB3 (unidades)
+        PORTD = (PORTD & 0x0F) | (decenas << 4);   // Actualizar decenas en PD4–PD7
+        PORTB = (PORTB & 0xF0) | (unidades & 0x0F); // Actualizar unidades en PB0–PB3
+       
         }
     }
