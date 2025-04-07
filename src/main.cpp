5a069152fff4ead85c60ffabbd605710a2361ac7
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-07 10:07:14 -0500

    aun errores en la resta

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@ int main(void) {
     DDRD |= 0xF0;            // PD4–PD7 como salida (decenas)
     DDRB |= 0x0F;            // PB0–PB3 como salida (unidades)
     
-    DDRD &= ~(0x04);     // PD3 como entrada (botón)
+    DDRD &= ~(0x04);     //  como entrada (botón)
     PORTD |= (0x04);     // Pull-up en PD3 (botón a GND)
+    
+    DDRD &= ~(0x08);     // como entrada (botón)
+    PORTD |= (0x08);     // Pull-up en PD3 (botón a GND)
+
 
     uint8_t unidades = 0;
     uint8_t decenas = 0;
@@ -34,6 +38,29 @@ int main(void) {
                         decenas = 0;
                     }
                 }
+            //para que retroceda pd3
+
+            if (!(PIND&0X08)) {     // Si el botón está presionado
+                _delay_ms(50);              // Anti-rebote
+    
+                // Esperar a que se suelte el botón
+                while (!(PIND & 0x08)); //mientras el boton este precionado el numero cambiara
+    
+                    if (unidades > 9) {
+                        _delay_ms(50);
+                        unidades = 0;
+                        decenas--;
+                    }
+                        else {
+                        _delay_ms(50);
+    
+                        unidades--;
+                        }
+                        if (decenas > 9) {
+                            _delay_ms(50);
+                            decenas = 0;
+                        }
+                    }
 
             // Mostrar en PD4–PD7 (decenas)
             PORTD = (PORTD & 0x0F) | (decenas << 4);
