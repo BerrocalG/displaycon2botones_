ecfbe53552bebb8b52c858167fcd10d0845be28a
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-08 16:16:35 -0500

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -6,70 +6,65 @@ int main(void) {
     DDRD |= 0xF0;            // PD4–PD7 como salida (decenas)
     DDRB |= 0x0F;            // PB0–PB3 como salida (unidades)
     
-    DDRD &= ~(0x04);     //  boton para subir
-    PORTD |= (0x04);     // Pull-up en PD3 (botón a GND)
-    
-    DDRD &= ~(0x08);     // boton para bajar
-    PORTD |= (0x08);     // Pull-up en PD4 (botón a GND)
+    DDRD &= ~(0x04);         // botón para subir
+    PORTD |= (0x04);         
+    DDRD &= ~(0x08);         // botón para bajar
+    PORTD |= (0x08);          
 
-    uint8_t unidades = 0; //declaramos la variable entero sin signo para poder contar en unidades
-    uint8_t decenas = 0; //declaramos las variables para descenas inia en 0 el valor
+    uint8_t unidades = 0;    
+    uint8_t decenas = 0;     
 
-    while (1) { //base programa
+    PORTD &= 0x0F;           
+    PORTB &= 0xF0;           
 
-        
-        //para el boton que sube numero de 1 en 1
+    while (1) {  // Bucle principal del programa
 
-        if (!(PIND&0X04)) {     // Si el botón está presionado
-            _delay_ms(50);              // Anti-rebote
+        // Si el botón para subir está presionado
+        if (!(PIND & 0x04)) {
+            _delay_ms(50);  // Anti-rebote
 
-            // Esperar a que se suelte el botón
-            while (!(PIND & 0x04)); //mientras el boton este precionado el numero cambiara
-                _delay_ms(50);              // Anti-rebote
+            // Espera hasta que se suelte el botón
+            while (!(PIND & 0x04)); 
+            _delay_ms(50);  // Anti-rebote
 
+            // Incrementar solo si no estamos en 99
+            if (decenas == 9 && unidades == 9) { // el valor se mantiene en 99 si ya está en ese valor
+            } else {
                 if (unidades >= 9) {
                     unidades = 0;
-                    decenas++; //sube en una unidad
-                }
-                    else {
-                    unidades++; //sube en una unidad
-
-                    }
-                    if (decenas > 9) {
-                        decenas = 0; // Asegurarse que las decenas no superen 9
-                    }
-                }
-
-            //para que retroceda pd3
-
-            if (!(PIND & 0x08)) {     // Si el botón en PD3 está presionado
-                _delay_ms(50);              // Anti-rebote
-    
-                // Esperar a que se suelte el botón
-                while (!(PIND & 0x08)); // Esperar a que se suelte el botón
-                _delay_ms(50);
-
-                if (!(unidades == 0 && decenas == 0)) { // esto es para que se quede firme en 00 y no de vuelta el de unidad
-
-                    if (unidades == 0) {
-                        unidades = 9;    // Si las unidades están en 0, reiniciar a 9
-                        
-                        if (decenas > 0) {
-                            decenas--;   // Decrementa las decenas si son mayores que 0
-                        }
-                    } else {
-                        unidades--;     // disminuye la unidad
-                    }
-        
-                    if (decenas > 9) {
-                        decenas = 0;    // Asegurarse que las decenas no superen 9
-                    }
-
+                    decenas++;  
+                } else {
+                    unidades++;  
                 }
             }
-            // Mostrar en PD4–PD7 (decenas) y en PB0–PB3 (unidades)
-        PORTD = (PORTD & 0x0F) | (decenas << 4);   // Actualizar decenas en PD4–PD7
-        PORTB = (PORTB & 0xF0) | (unidades & 0x0F); // Actualizar unidades en PB0–PB3
-       
+
+            if (decenas > 9) {
+                decenas = 9;
+            }
         }
+
+        // Si el botón para bajar está presionado
+        if (!(PIND & 0x08)) {
+            _delay_ms(50);  // anti-rebote
+
+            // espera hasta que se suelte el botón
+            while (!(PIND & 0x08)); 
+            _delay_ms(50);  // annti-rebote
+
+            if (!(unidades == 0 && decenas == 0)) {  // paraevitar decrecer cuando este en 00
+                if (unidades == 0) {
+                    unidades = 9;  
+                    if (decenas > 0) {
+                        decenas--;  
+                    }
+                } else {
+                    unidades--;  
+                }
+            }
+        }
+
+        PORTD = (PORTD & 0x0F) | (decenas << 4);   
+        PORTB = (PORTB & 0xF0) | (unidades & 0x0F); 
     }
+}
+
